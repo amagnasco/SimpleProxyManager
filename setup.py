d24160159c59cab91c53a7e1e82c4b87bb6fe628
Alessandro G. Magnasco <amagnasco@gradcenter.cuny.edu>
2024-04-24 18:13:41 -0500

    versioning

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='proxyManager',
-   version='1.0',
+   version='0.0.1',
    description='Simple Python multithreaded proxy manager, focused on scrapes',
    license="CC-BY-SA-4.0",
    long_description=long_description,
