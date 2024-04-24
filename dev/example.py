a72dd7e5990494e47c253f327f6cf8baed0663c2
Alessandro G. Magnasco <amagnasco@gradcenter.cuny.edu>
2024-04-24 18:09:04 -0500

    structure

--- dev/example.py
+++ dev/example.py
@@ -1,45 +1,53 @@
+# this is an example file for development purposes
+
+### IMPORTS
+
 import os
 import asyncio
-from proxyManager import ProxyManager
+from src import ProxyManager
 
-# lista de proxies
-filename = "testproxies.txt"
+### SETTINGS
 
-# numero de hilos a usar
+# list of proxies
+filename = "proxies.txt"
+
+# number of threads to use
 threads = 10
 
-# tiempo de espera en segundos
+# wait time in seconds
 wait = {
     "min": 3,
     "max": 8
 }
 
-# headers
+# query headers
 headers = {
     "ua": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0',
     "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     "accept_language": 'en-US,en;q=0.5'
 }
 
-# configuracion para health check
+# configuration for health check
 test = {
     "uri": "http://ipinfo.io/json",
     "min": 1,
     "max": 3
 }
 
-### workdir
+### USAGE
+
+# set workdir
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 filepath = os.path.join(__location__, filename)
 
-### start
+# start
 proxies = ProxyManager(threads, wait, headers, test)
 await proxies.load(filepath)
 
 # test
 proxies.available()
 
-# try to scrape an URL:
+# query an URL:
 #url = "http://example.com"
 #proxies.req(url)
 
---
+++ setup.py
@@ -0,0 +1,21 @@
+# do not call this file directly, instead use pip install
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='proxyManager',
+   version='1.0',
+   description='Simple Python multithreaded proxy manager, focused on scrapes',
+   license="CC-BY-SA-4.0",
+   long_description=long_description,
+   author='Alessandro G. Magnasco',
+   author_email='amagnasco@gradcenter.cuny.edu',
+   url="http://www.github.com/amagnasco/proxyManager",
+   packages=['proxyManager'],
+   install_requires=['threading', 'queue', 'asyncio', 'urllib', 'random', 'time'],
+   dev=[
+            'dev/example',
+           ]
+)
